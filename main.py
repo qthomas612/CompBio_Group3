2aa65b9212aebe49e81a3f17829baf3ecf253bf7
Jared-Tomei <99999382+Jared-Tomei@users.noreply.github.com>
2022-04-03 21:06:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+Import OS
+##This should start the docker and mount it. Will be etider later in order to have the mount lead to a drive on any users instead of hard coding
+os.system("docker run --mount type=bind,source=/home/qthomas/data,target=/data -it plasmid:latest bash")
+##Testing for plasforest
+os.system("docker exec ./test_plasforest.sh")
+##using plasforest
+os.system("docker exec python3 PlasForest.py -i data/")
 
