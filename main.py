239d2a4b571218c6e5fae3361c883acb1dc28a39
Jared-Tomei <99999382+Jared-Tomei@users.noreply.github.com>
2022-04-04 17:17:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import os
 ##Theortical, do not attempt to use before extenstive bug fixing)
 
+import os, argparse, multiprocessing
 
+ap = argparse.ArgumentParser()
+ap.add_argument('-t','--threads', type = int, default = multiprocessing.cpu_count()/2, help = 'Number of CPUs to use in computation. Defaults to half the number of available CPUs', metavar = '')
+ap.add_argument('-s','--seqtype', type = str, default = "illumina", help = 'Type of sequences. Options are illumina or nanopore. The illumina option works for all short reads', metavar = '')
+ap.add_argument('-r','--trim', type = str, default = False, help = 'Trim adapter sequences using trimmomatic. Accpets a string of the adapter to trim', metavar = '')
+ap.add_argument('-d','--data', type = str, required = True,  help = 'input data', metavar = '')
+ap.add_argument('-o','--output', type = str, default = "./output", help = 'directory to write results to', metavar = '')
+
+args = vars(ap.parse_args())
+
+assert os.path.isfile(args['data']), "Could not find the file {}".format(args['data'])
 
 
 #Trimmomatic
@@ -17,4 +28,4 @@ os.system("python3 PlasForest.py -i /data/")
 ##platon database should be set up upon creation of the container
 #!Alert results need to be changed to allow output from docker container to the users output.
 os.system("docker exec platon --db ~/db --output results/ --verbose  /data/U54_LUC_01180.nanopore.fastq")
-#Need to address if we are using SPAdes or plasmidSPAdes for input into recycler before wiritng the code to do it
+
