4b33238ca16f92adf1d2ba0546331111f6cbe492
Jared-Tomei <99999382+Jared-Tomei@users.noreply.github.com>
2022-04-10 13:07:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,9 @@ assert os.path.isfile(args['data']), "Could not find the file {}".format(args['d
 
 
 #Trimmomatic
+#if -r == True:
+    #os.system(java -jar trimmomatic-0.35.jar SE -phred33 -d -o ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36)
+
 
 
 #Need to run spades first
