c34456f5ee2261f36e1caad912ecfed122d1327d
qthomas612 <72045942+qthomas612@users.noreply.github.com>
2022-04-11 17:07:39 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+#Take user input flags
 while getopts t:s:r:d:o:p: flag
 do
     case "${flag}" in
@@ -9,10 +11,24 @@ do
         d) data=${OPTARG};;
         o) output=${OPTARG};;
         p) readtype=${OPTARG};;
+       \?) echo "Error: Invalid option"
+           exit;;
     esac
 done
 
+#Make sure user input a data file, if not exit script
+if [ -z "$data" ]; then
+        echo 'Missing -d' >&2
+        exit 1
+fi
 
+#If the user input a trim option then call trimmomatic
+if $trim
+then
+  java -jar trimmomatic-0.35.jar SE -phred33 -d -o ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+fi
+
+#if user input threads
 spades.py -s data/U54_LUC_01180.nanopore.fastq -o data/SpadesResults
 
 
