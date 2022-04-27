b664bd9e6533b23b66f8aa95331a279df5e6f50c
Kosciuch <tristankosciuch@gmail.com>
2022-04-27 22:49:31 +0000

    paths in main.sh

--- docker/main.sh
+++ docker/main.sh
@@ -7,30 +7,15 @@ do
     case "${flag}" in
         t) threads=${OPTARG};;
         s) seqtype=${OPTARG};;
-        r) trim=${OPTARG};;
         d) data=${OPTARG};;
-        p) readtype=${OPTARG};;
+        r) readtype=${OPTARG};;
        \?) echo "Error: Invalid option"
            exit;;
     esac
 done
 
-## TK: couldn't get the defaults to work here and defaults are handled in plasmid.py
-#set defaults if options are empty
-#${threads:-4}
-#${seqtype:-illumina}
-#${output:-./output}
-#${readtype:-unpaired}
-
-## TK: will check this in plasmid.py
-#Make sure user input a data file, if not exit script
-#if [ -z "$data" ]; then
-#        echo 'Missing -d' >&2
-#        exit 1
-#fi
-
 #make output directory of results
-mkdir output output/Results output/Results/recycler output/Results/plasforest output/Results/platon output/Results/SPAdes output/Results/plasmidSPAdes
+mkdir /output/Results /output/Results/recycler /output/Results/plasforest /output/Results/platon /output/Results/SPAdes /output/Results/plasmidSPAdes
 
 
 #RUN THIS COMMAND WITH USER INPUTS
@@ -39,34 +24,33 @@ spades.py -s /data/$data -o /output/Results/SPAdes
 
 #SPADES PLASMID PREDICTION
 #double check this command is right || Command is right and runs without errors
-spades.py --plasmid -s /data/$data -o data/Results/plasmidSPAdes
+spades.py --plasmid -s /data/$data -o /output/Results/plasmidSPAdes
 
 #PLASFOREST
 #plasforest app has all the paths to needed files hardcoded in their main file so redirect to PlasForest directory and return after using it.
-cd ~/PlasForest/
 #run plasForest
-PlasForest.py -i ../../data/Results/SPAdes/contigs.fasta  -o ../../data/Results/plasforest/plasforestResults.csv
+PlasForest.py -i /output/Results/SPAdes/contigs.fasta  -o /output/Results/plasforest/plasforestResults.csv
 
 #return to previous directory
-cd ../../
+#cd ../../ # TK: I don't think we need to move around directories as all the tools should be in $PATH
 
 #PLATON
-platon --db ~/db --output output/Results/platon/ --verbose --threads $threads /output/Results/SPAdes/contigs.fasta
+platon --db ~/db --output /output/Results/platon/ --verbose --threads $threads /output/Results/SPAdes/contigs.fasta
 
 
-#RECYCLER
-conda activate recycler
-make_fasta_from_fastg.py -g /output/SpadesResults/assembly_graph.fastg -o /output/recycler/assembly_graph.nodes.fasta
-bwa index /output/recycler/assembly_graph.nodes.fasta
-bwa mem /output/recycler/assembly_graph.nodes.fasta /output/recycler/R1.fastq.gz /output/recycler/R2.fastq.gz | samtools view -buS - > /output/recycler/reads_pe.bam
-samtools view -bF 0x0800 /output/recycler/reads_pe.bam > /output/recycler/reads_pe_primary.bam
-samtools sort /output/recycler/reads_pe_primary.bam -o /output/recycler/reads_pe_primary.sort.bam
-samtools index /output/recycler/reads_pe_primary.sort.bam
+#RECYCLER # TK: commented out bc its not currently working
+#conda activate recycler
+#make_fasta_from_fastg.py -g /output/SpadesResults/assembly_graph.fastg -o /output/recycler/assembly_graph.nodes.fasta
+#bwa index /output/recycler/assembly_graph.nodes.fasta
+#bwa mem /output/recycler/assembly_graph.nodes.fasta /output/recycler/R1.fastq.gz /output/recycler/R2.fastq.gz | samtools view -buS - > /output/recycler/reads_pe.bam
+#samtools view -bF 0x0800 /output/recycler/reads_pe.bam > /output/recycler/reads_pe_primary.bam
+#samtools sort /output/recycler/reads_pe_primary.bam -o /output/recycler/reads_pe_primary.sort.bam
+#samtools index /output/recycler/reads_pe_primary.sort.bam
 
 # find largest kmer used by spades
 
 
-recycle.py -g /output/recycler/assembly_graph.fastg -k 55 -b /output/recycler/reads_pe_primary.sort.bam -i True -o /output/recycler/
+#recycle.py -g /output/recycler/assembly_graph.fastg -k 55 -b /output/recycler/reads_pe_primary.sort.bam -i True -o /output/recycler/
 # need to figure out what spades used for k value
 #SPAdes utilizes multisized de Bruijn graph which allows employing different values of k. That is what the K<##> in resutls related to, so we have no set k-value
 
--- plasmid.py
+++ plasmid.py
@@ -5,9 +5,9 @@ import os, argparse, multiprocessing
 ap = argparse.ArgumentParser()
 ap.add_argument('-t','--threads', type = int, default = 4, help = 'Number of CPUs to use in computation. Defaults to half the number of available CPUs', metavar = '')
 ap.add_argument('-s','--seqtype', type = str, default = "illumina", help = 'Type of sequences. Options are illumina or nanopore. The illumina option works for all short reads', metavar = '')
-ap.add_argument('-d','--data', type = str, required = True,  help = 'input data', metavar = '')
+ap.add_argument('-f','--file', type = str, required = True,  help = 'input fasta file', metavar = '')
 ap.add_argument('-o','--output', type = str, default = "./output", help = 'directory to write results to', metavar = '')
-ap.add_argument('-l','--readtype', type = str, default = False, help = "Type of reads. Options are 'interlaced forward', 'reverse paired-end', or 'forward paired-end'", metavar = '')
+ap.add_argument('-p','--readtype', type = str, default = False, help = "Type of reads. Options are 'interlaced forward', 'reverse paired-end', or 'forward paired-end'", metavar = '')
 
 args = vars(ap.parse_args())
 
@@ -15,11 +15,11 @@ assert os.path.isfile(args['data']), "Could not find the file {}".format(args['d
 
 os.makedirs(args['output'],exist_ok = True)
 
-data =  os.path.realpath(args['data'])
-data_target = os.path.basename(args['data'])
+data =  os.path.realpath(args['file'])
+data_target = os.path.basename(args['file'])
 output = os.path.realpath(args['output'])
 
-docker_str = "docker run --mount type=bind,source="+data+",target=/data/"+data_target+" --mount type=bind,source="+output+",target=/output plasmid:latest -t "+str(args['threads'])+" -s "+args["seqtype"]+" -d "+data_target
+docker_str = "docker run --rm --mount type=bind,source="+data+",target=/data/"+data_target+" --mount type=bind,source="+output+",target=/output plasmid:latest /usr/local/bin/main.sh -t "+str(args['threads'])+" -s "+args["seqtype"]+" -f "+data_target
 
 if args['trim']:
     docker_str += " -r "+args['trim']
