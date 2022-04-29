117e24babf4356ef0181909dd38407a76c67c2d4
Kosciuch <tristankosciuch@gmail.com>
2022-04-29 16:43:30 +0000

    file to format results

--- docker/compile_results.py
+++ docker/compile_results.py
@@ -3,29 +3,30 @@
 from Bio.Blast.Applications import NcbiblastnCommandline 
 from Bio.Blast import NCBIXML
 from Bio import SeqIO 
-import os
+import os, numpy
+
+
+
+
+
+
+
 
 
 # create a blast database of SPAdes contigs
-os.system("makeblastdb -in /output/Results/SPAdes/contigs.fasta -parse_seqids -dbtype nucl -out /output/Results/blast_db/SPADes")
+os.system("makeblastdb -in /output/Results/SPAdes/contigs.fasta -parse_seqids -dbtype nucl -out /output/Results/blast_db/SPAdes")
 
-blastn_cline = NcbiblastnCommandline(query = "/output/Results/plasmidSPAdes/contigs.fasta", db = "/output/Results/blast_db/SPADes", outfmt = 5, out = "/output/plasmidSPAdes/plasmidSPAdes_blast.xml") 
+blastn_cline = NcbiblastnCommandline(query = "/output/Results/plasmidSPAdes/contigs.fasta", db = "/output/Results/blast_db/SPAdes", outfmt = 5, out = "/output/plasmidSPAdes/plasmidSPAdes_blast.xml") 
 
 stdout, stderr = blastn_cline()
 
-results = dict()
+results = [["plasmidSPAdes plasmid","closest SPAdes contig"]]
 
-for record in NCBIXML.parse(open("results.xml")): 
+for record in NCBIXML.parse(open("/output/plasmidSPAdes/plasmidSPAdes_blast.xml")): 
     if record.alignments:
-        record.alignments.hsps.expect
-
-
-        results[record.query]
-
-        print("\n") 
-        print("query: %s" % record.query[:100]) 
-        for align in record.alignments:
-            minE = min()
-            for hsp in align.hsps:
-                if hsp.expect < E_VALUE_THRESH: 
-                    print("match: %s " % align.title[:100])
+        query = record.query.split("_component_")[0]
+        match = record.alignments[0].title.split(" No definition line")[0]
+        results.append([query,match])
+        #results[record.query] = record.alignments[0].title
+results
+numpy.savetxt("/output/plasmidSPAdes_closest_conitg.csv", results, delimiter =", ", fmt ='% s')
