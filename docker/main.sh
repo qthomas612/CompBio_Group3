38e1116fd73caa09154f533dbe85dfa985a0ae5f
Kosciuch <tristankosciuch@gmail.com>
2022-05-01 04:43:39 +0000

    parallelized execution

--- docker/main.sh
+++ docker/main.sh
@@ -2,13 +2,14 @@
 
 
 #Take user input flags
-while getopts t:s:r:d:o:p: flag
+while getopts t:r:d:1:2: flag
 do
     case "${flag}" in
         t) threads=${OPTARG};;
-        s) seqtype=${OPTARG};;
         d) data=${OPTARG};;
         r) readtype=${OPTARG};;
+        1) forward=${OPTARG};;
+        2) reverseto=${OPTARG};;
        \?) echo "Error: Invalid option"
            exit;;
     esac
@@ -17,25 +18,42 @@ done
 #make output directory of results
 mkdir /output/Results /output/Results/blast_db /output/Results/recycler /output/Results/plasforest /output/Results/platon /output/Results/SPAdes /output/Results/plasmidSPAdes
 
-#RUN THIS COMMAND WITH USER INPUTS
 #SPADES ASSEMBLY
-spades.py -s /data/$data -o /output/Results/SPAdes
+if [$readtype = "12"]
+then
+    spades.py --12 /data/$data -o /output/Results/SPAdes & SPADES=$!
+else
+    spades.py -$readtype /data/$data -o /output/Results/SPAdes & SPADES=$!
+fi
 
 #SPADES PLASMID PREDICTION
 #double check this command is right || Command is right and runs without errors
-spades.py --plasmid -s /data/$data -o /output/Results/plasmidSPAdes
+#spades.py --plasmid -s /data/$data -o /output/Results/plasmidSPAdes
+
+if [$readtype = 12]
+then
+    spades.py --plasmid --12 /data/$data -o /output/Results/plasmidSPAdes &
+else
+    spades.py --plasmid -$readtype /data/$data -o /output/Results/plasmidSPAdes &
+fi
+
+wait $SPADES
 
 #PLASFOREST
 #plasforest app has all the paths to needed files hardcoded in their main file so redirect to PlasForest directory and return after using it.
 #run plasForest
-PlasForest.py -i /output/Results/SPAdes/contigs.fasta  -o /output/Results/plasforest/plasforestResults.csv
+PlasForest.py -i /output/Results/SPAdes/contigs.fasta  -o /output/Results/plasforest/plasforestResults.csv &
 
 #return to previous directory
 #cd ../../ # TK: I don't think we need to move around directories as all the tools should be in $PATH
 
 #PLATON
-platon --db ~/db --output /output/Results/platon/ --verbose --threads $threads /output/Results/SPAdes/contigs.fasta
+platon --db ~/db --output /output/Results/platon/ --verbose --threads $threads /output/Results/SPAdes/contigs.fasta &
 
+wait
+
+# format results
+/root/compile_results.py
 
 #RECYCLER # TK: commented out bc its not currently working
 #conda activate recycler
