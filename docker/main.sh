9b270123e07bfa1dae15986b0683faf619368010
qthomas612 <72045942+qthomas612@users.noreply.github.com>
2022-05-04 15:01:31 -0500

    Update main.sh

--- docker/main.sh
+++ docker/main.sh
@@ -16,17 +16,17 @@ do
 done
 
 #make output directory of results
-mkdir /output/Results /output/Results/blast_db /output/Results/recycler /output/Results/plasforest /output/Results/platon /output/Results/SPAdes /output/Results/plasmidSPAdes
+mkdir ./output/Results ./output/Results/blast_db ./output/Results/plasforest ./output/Results/platon ./output/Results/SPAdes ./output/Results/plasmidSPAdes
 
 #SPADES ASSEMBLY
 if [$readtype = "12"]
 then
-    spades.py -t $threads --12 /data/$file -o /output/Results/SPAdes
+    spades.py -t $threads --12 ./data/$file -o ./output/Results/SPAdes
 elif [$readtype = "1+2"]
 then
-    spades.py -t $threads -1 /data/$1 -1 /data/$2 -o /output/Results/SPAdes
+    spades.py -t $threads -1 ./data/$1 -2 ./data/$2 -o ./output/Results/SPAdes
 else
-    spades.py -t $threads -$readtype /data/$file -o /output/Results/SPAdes
+    spades.py -t $threads -$readtype ./data/$file -o ./output/Results/SPAdes
 fi
 
 #SPADES PLASMID PREDICTION
@@ -35,26 +35,26 @@ fi
 
 if [$readtype = "12"]
 then
-    spades.py -t $threads --plasmid --12 /data/$file -o /output/Results/plasmidSPAdes
+    spades.py -t $threads --plasmid --12 ./data/$file -o ./output/Results/plasmidSPAdes
 elif [$readtype = "1+2"]
 then
-    spades.py -t $threads --plasmid -1 /data/$1 -1 /data/$2 -o /output/Results/plasmidSPAdes
+    spades.py -t $threads --plasmid -1 ./data/$1 -2 ./data/$2 -o ./output/Results/plasmidSPAdes
 else
-    spades.py -t $threads --plasmid -$readtype /data/$file -o /output/Results/plasmidSPAdes
+    spades.py -t $threads --plasmid -$readtype ./data/$file -o ./output/Results/plasmidSPAdes
 fi
 
 #PLASFOREST
 #plasforest app has all the paths to needed files hardcoded in their main file so redirect to PlasForest directory and return after using it.
 #run plasForest
 cd /root/PlasForest
-PlasForest.py --threads $threads -i /output/Results/SPAdes/contigs.fasta  -o /output/Results/plasforest/plasforestResults.csv
+PlasForest.py --threads $threads -i ./output/Results/SPAdes/contigs.fasta  -o ./output/Results/plasforest/plasforestResults.csv
 cd
 
 #return to previous directory
 #cd ../../ # TK: I don't think we need to move around directories as all the tools should be in $PATH
 
 #PLATON
-platon --threads $threads --db ~/db --output /output/Results/platon/ /output/Results/SPAdes/contigs.fasta
+platon --threads $threads --db ~/db --output ./output/Results/platon/ ./output/Results/SPAdes/contigs.fasta
 
 # format results
 /root/compile_results.py
